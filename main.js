28b9018141539aefcf5dad5c7834e9f884cbd3cd
Naman2708 <87568378+Naman2708@users.noreply.github.com>
2023-06-16 15:57:58 +0530

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-peter_pan_song = "";
+Peter_pan_song = "";
 Harry_Potter_Theme_song = "";
+leftWrist_x = 0;
+leftWrist_y = 0;
+rightWrist_x = 0;
+rightWrist_y = 0; 
 
 function preload() {
     peter_pan_song = loadSound("music2.mp3");
@@ -12,8 +16,32 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video,0,0,600,530);
 }
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = " + leftWrist_x +" leftWrist_y = "+ leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = " + rightWrist_x +" rightWrist_y = "+ rightWrist_y);
+    }
+}
+
